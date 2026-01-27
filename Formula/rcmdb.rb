9b25a3b5def63dc7f061c26df1a4d8383dce849d
vlwkaos <mgparkprint@gmail.com>
2026-01-27 14:09:04 +0900

    chore: update rcmdb to v0.2.2
    SHA256: f6a85e6fda88da5555423c006f7b12a8930f10e7503b27f77bd97f5f0838f85e

--- Formula/rcmdb.rb
+++ Formula/rcmdb.rb
@@ -1,9 +1,9 @@
 class Rcmdb < Formula
   desc "TUI for Karabiner-Elements right_command key bindings"
   homepage "https://github.com/vlwkaos/karabiner-rcmd-binder"
-  url "https://github.com/vlwkaos/karabiner-rcmd-binder/releases/download/v0.2.1/rcmdb-0.2.1-macos-arm64.tar.gz"
-  sha256 "57584619aa95f470eedd7eeaf8156d488e7c84e08c27e69869045692b55fe6e3"
-  version "0.2.1"
+  url "https://github.com/vlwkaos/karabiner-rcmd-binder/releases/download/v0.2.2/rcmdb-0.2.2-macos-arm64.tar.gz"
+  sha256 "f6a85e6fda88da5555423c006f7b12a8930f10e7503b27f77bd97f5f0838f85e"
+  version "0.2.2"
   
   depends_on :macos
   
