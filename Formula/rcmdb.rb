322099256a420f8715cb34bb420a6b8c8c83fc25
vlwkaos <mgparkprint@gmail.com>
2026-01-29 15:30:37 +0900

    rcmdb 0.2.3

--- Formula/rcmdb.rb
+++ Formula/rcmdb.rb
@@ -1,27 +1,15 @@
 class Rcmdb < Formula
-  desc "TUI for Karabiner-Elements right_command key bindings"
+  desc "TUI for configuring Karabiner-Elements right_command key bindings"
   homepage "https://github.com/vlwkaos/karabiner-rcmd-binder"
-  url "https://github.com/vlwkaos/karabiner-rcmd-binder/releases/download/v0.2.2/rcmdb-0.2.2-macos-arm64.tar.gz"
-  sha256 "f6a85e6fda88da5555423c006f7b12a8930f10e7503b27f77bd97f5f0838f85e"
-  version "0.2.2"
-  
-  depends_on :macos
-  
+  url "https://github.com/vlwkaos/karabiner-rcmd-binder/releases/download/v0.2.3/rcmdb-0.2.3-darwin-arm64.tar.gz"
+  sha256 "be12b5ff22ee6b4cf41a2250a0f6cb01be9566e7a988e6592d0488c544731df8"
+  license "MIT"
+
   def install
     bin.install "rcmdb"
   end
-  
-  def caveats
-    <<~EOS
-      Configuration stored in: ~/.config/karabiner-rcmd-binder/config.toml
-      
-      To verify GPG signature:
-        curl -sL https://github.com/vlwkaos.gpg | gpg --import
-        gpg --verify rcmdb-*.tar.gz.asc
-    EOS
-  end
-  
+
   test do
-    assert_match "rcmdb", shell_output("#{bin}/rcmdb --version 2>&1", 1)
+    system "#{bin}/rcmdb", "--version"
   end
 end
