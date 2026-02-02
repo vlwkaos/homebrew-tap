f1ccd2ea04ee2c2bd1fd18ba9a12207b0ffb9ff6
vlwkaos <mgparkprint@gmail.com>
2026-02-02 19:31:51 +0900

    gifer 0.2.1

--- Formula/gifer.rb
+++ Formula/gifer.rb
@@ -1,8 +1,8 @@
 class Gifer < Formula
   desc "TUI application for converting videos to GIFs"
   homepage "https://github.com/vlwkaos/gifer"
-  url "https://github.com/vlwkaos/gifer/releases/download/v0.2.0/gifer-0.2.0-darwin-arm64.tar.gz"
-  sha256 "a59b4df43fe7b660194a2232c7a60e9528f101edcf1e5749454ebd29e56dc2c3"
+  url "https://github.com/vlwkaos/gifer/releases/download/v0.2.1/gifer-0.2.1-darwin-arm64.tar.gz"
+  sha256 "a97d094cf1575e29ed3fadb3df9c53f5d45de0bb97e979fb192ff2ce5d358f09"
   license "MIT"
 
   def install
