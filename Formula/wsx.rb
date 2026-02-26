c2a6154eea290b4b131b903d902c4abc0c89bbf7
vlwkaos <mgparkprint@gmail.com>
2026-02-26 13:52:33 +0900

    wsx 0.4.2

--- Formula/wsx.rb
+++ Formula/wsx.rb
@@ -1,9 +1,9 @@
 class Wsx < Formula
   desc "TUI workspace manager for git worktrees and tmux sessions"
   homepage "https://github.com/vlwkaos/wsx"
-  url "https://github.com/vlwkaos/wsx/releases/download/v0.4.1/wsx-0.4.1-darwin-arm64.tar.gz"
-  version "0.4.1"
-  sha256 "e34e4bad5444cf1e45bd9f76055162f4394ced162b231d0267f92f0491ad14b1"
+  url "https://github.com/vlwkaos/wsx/releases/download/v0.4.2/wsx-0.4.2-darwin-arm64.tar.gz"
+  version "0.4.2"
+  sha256 "1b974f29bcdc4b1c965b4b949a88759575a367f86a931654a43cf544ce60edf7"
   license "MIT"
 
   def install
