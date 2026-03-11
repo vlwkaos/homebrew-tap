f4d5a150b7015ab009743c0642d2589e37741446
vlwkaos <mgparkprint@gmail.com>
2026-03-11 16:15:00 +0900

    skiller 0.1.0

--- Formula/skiller.rb
+++ Formula/skiller.rb
@@ -3,7 +3,7 @@ class Skiller < Formula
   homepage "https://github.com/vlwkaos/skiller"
   url "https://github.com/vlwkaos/skiller/releases/download/v0.1.0/skiller-0.1.0-darwin-arm64.tar.gz"
   version "0.1.0"
-  sha256 "9b45291b318fe86989c871bd8950d24d9c11749fca75425d4077cab11b9a43d4"
+  sha256 "ebc0b9c9503f1b9b72b85d44cf5b1a0ee3656c8bdcf40c05b7a669a60ceee551"
   license "MIT"
 
   def install
