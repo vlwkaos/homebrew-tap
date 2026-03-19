8490109a3749541489225ac17bf945d4396a8f5a
vlwkaos <mgparkprint@gmail.com>
2026-03-19 17:54:30 +0900

    ir 0.6.1

--- Formula/ir.rb
+++ Formula/ir.rb
@@ -1,9 +1,9 @@
 class Ir < Formula
   desc "Local markdown semantic search with hybrid BM25+vector retrieval and LLM reranking"
   homepage "https://github.com/vlwkaos/ir"
-  url "https://github.com/vlwkaos/ir/releases/download/v0.6.0/ir-0.6.0-darwin-arm64.tar.gz"
-  version "0.6.0"
-  sha256 "ac220c2a5244635e07700d7d66dc241fc8aa57ce0095245a1f602ad9e2c5f933"
+  url "https://github.com/vlwkaos/ir/releases/download/v0.6.1/ir-0.6.1-darwin-arm64.tar.gz"
+  version "0.6.1"
+  sha256 "3a13aaccd497a0ce090d19e342c41ff85578924ec36235d4547ca9ee49c221b3"
   license "MIT"
 
   def install
