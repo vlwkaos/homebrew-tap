e5b58721fb491e5605ab09c41787c95b4469c288
vlwkaos <mgparkprint@gmail.com>
2026-04-02 00:22:49 +0900

    skiller 0.2.2

--- Formula/skiller.rb
+++ Formula/skiller.rb
@@ -1,14 +1,14 @@
 class Skiller < Formula
   desc "Symlink manager for AI tool skill bundles"
   homepage "https://github.com/vlwkaos/skiller"
-  url "https://github.com/vlwkaos/skiller/releases/download/v0.2.1/skiller-0.2.1-darwin-universal.tar.gz"
-  version "0.2.1"
-  sha256 "4956bf243b1dcc171228b7ba3640077498ae4733aec49c41bcbda42f4a6e98d3"
+  url "https://github.com/vlwkaos/skiller/releases/download/v0.2.2/skiller-0.2.2-darwin-universal.tar.gz"
+  version "0.2.2"
+  sha256 "6e21e394af2accfd736af4dfc29873498985b145d57ac9d023d4d1620d8636e3"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/vlwkaos/skiller/releases/download/v0.2.1"
-    sha256 cellar: :any_skip_relocation, all: "ab7798e311c35e8bd2200bc15972c2538da728cc534420a3a0418d05f99aa6c2"
+    root_url "https://github.com/vlwkaos/skiller/releases/download/v0.2.2"
+    sha256 cellar: :any_skip_relocation, all: "00d3b525adf3bfa0331c71addcd34af10a4629883256d892c55aa00679cf5e30"
   end
 
   def install
