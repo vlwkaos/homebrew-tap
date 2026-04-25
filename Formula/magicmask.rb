092591508ba33c29b49a3f591c42f4ae6e399b6f
vlwkaos <mgparkprint@gmail.com>
2026-04-26 07:57:24 +0900

    magicmask 0.1.3

--- Formula/magicmask.rb
+++ Formula/magicmask.rb
@@ -1,9 +1,9 @@
 class Magicmask < Formula
   desc "Menu bar utility for global Magic Trackpad crop suppression"
   homepage "https://github.com/vlwkaos/MagicMask"
-  url "https://github.com/vlwkaos/MagicMask/releases/download/v0.1.2/magicmask-v0.1.2-macos-universal.tar.gz"
-  version "0.1.2"
-  sha256 "639e61ca0a07d791b8ec8b8952bcc80f2aaba5b7894b9c7180e92e71a9757c17"
+  url "https://github.com/vlwkaos/MagicMask/releases/download/v0.1.3/magicmask-v0.1.3-macos-universal.tar.gz"
+  version "0.1.3"
+  sha256 "006ea2944b83b8a2a0a851981c75762d679581ef607286360492b675bc613042"
   license "MIT"
 
   def install
