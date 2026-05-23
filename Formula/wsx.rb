e768004363f496dddf3241cb4e8fc35293f0468e
vlwkaos <mgparkprint@gmail.com>
2026-05-23 18:16:10 +0900

    wsx 0.15.9

--- Formula/wsx.rb
+++ Formula/wsx.rb
@@ -1,14 +1,14 @@
 class Wsx < Formula
   desc "TUI workspace manager — git worktrees + tmux sessions in one tree"
   homepage "https://github.com/vlwkaos/wsx"
-  url "https://github.com/vlwkaos/wsx/releases/download/v0.15.7/wsx-0.15.7-darwin-universal.tar.gz"
-  version "0.15.7"
-  sha256 "0ccdad22114b2859e782853eab943aa4dc5478f33792b61fa05ebd9a8cb6e3a7"
+  url "https://github.com/vlwkaos/wsx/releases/download/v0.15.9/wsx-0.15.9-darwin-universal.tar.gz"
+  version "0.15.9"
+  sha256 "45d02cc1c57c47acf17b5184b0559f29a6c456546ff964025ccd69bebf95550a"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/vlwkaos/wsx/releases/download/v0.15.7"
-    sha256 cellar: :any_skip_relocation, all: "606cbc33e29aee385492a611e3101ec667087d84bd94a00dec37253346cef998"
+    root_url "https://github.com/vlwkaos/wsx/releases/download/v0.15.9"
+    sha256 cellar: :any_skip_relocation, all: "9297d9013859a992fba2dfdb0ff0a6a9a1ad0db4e607be525a877be58e08d01a"
   end
 
   def install
