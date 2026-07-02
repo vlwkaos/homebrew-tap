778e67ecb40207c0b9d8d6e6811810afa2da60b0
vlwkaos <mgparkprint@gmail.com>
2026-07-02 14:30:43 +0900

    rcmdb 0.6.0

--- Formula/rcmdb.rb
+++ Formula/rcmdb.rb
@@ -1,10 +1,15 @@
 class Rcmdb < Formula
   desc "TUI for configuring Karabiner-Elements right_command key bindings"
   homepage "https://github.com/vlwkaos/karabiner-rcmd-binder"
-  url "https://github.com/vlwkaos/karabiner-rcmd-binder/releases/download/v0.5.2/rcmdb-0.5.2-macos-arm64.tar.gz"
-  version "0.5.2"
-  sha256 "f386c05f9be5ccd1d972939317ad1465d964cc5c1ec516028fd7a92252eb928a"
+  url "https://github.com/vlwkaos/karabiner-rcmd-binder/releases/download/v0.6.0/rcmdb-0.6.0-darwin-universal.tar.gz"
+  version "0.6.0"
+  sha256 "34474b0b2a3987cc5210ac3499749e72945fceaf6ab77ed407aae73bbecb8f43"
   license "MIT"
+
+  bottle do
+    root_url "https://github.com/vlwkaos/karabiner-rcmd-binder/releases/download/v0.6.0"
+    sha256 cellar: :any_skip_relocation, all: "b380e8f4a80f35880f96f92711b78417d5e3dacface01ce53c7994b13798a728"
+  end
 
   def install
     bin.install "rcmdb"
