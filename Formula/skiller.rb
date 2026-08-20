d800a8eee569835057f9d3d136e0e798e07002b5
vlwkaos <mgparkprint@gmail.com>
2026-08-20 19:02:38 +0900

    fix(skiller): publish final 0.1.0 artifact

--- Formula/skiller.rb
+++ Formula/skiller.rb
@@ -1,8 +1,8 @@
 class Skiller < Formula
   desc "Declarative project and global Agent Skills manager"
   homepage "https://github.com/vlwkaos/skiller"
-  url "https://github.com/vlwkaos/skiller/releases/download/v0.1.0/skiller-0.1.0-darwin-universal-7be3914.tar.gz"
-  sha256 "7c632bc4d50b9572b4f11f152deae5ad8c069a0f51fd1cb0648c1e72658b9c51"
+  url "https://github.com/vlwkaos/skiller/releases/download/v0.1.0/skiller-0.1.0-darwin-universal-8dfb474.tar.gz"
+  sha256 "de50fc00ce6322097cf88bc64c638e610c2a303fc16af77e897ad3218eb9ef5f"
   license "MIT"
 
   def install
