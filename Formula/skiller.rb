3e7c6e6e2b2dcc79e2ca154740711a2c154ac758
vlwkaos <mgparkprint@gmail.com>
2026-08-20 18:57:21 +0900

    fix(skiller): use immutable release asset URL

--- Formula/skiller.rb
+++ Formula/skiller.rb
@@ -1,7 +1,7 @@
 class Skiller < Formula
   desc "Declarative project and global Agent Skills manager"
   homepage "https://github.com/vlwkaos/skiller"
-  url "https://github.com/vlwkaos/skiller/releases/download/v0.1.0/skiller-0.1.0-darwin-universal.tar.gz"
+  url "https://github.com/vlwkaos/skiller/releases/download/v0.1.0/skiller-0.1.0-darwin-universal-7be3914.tar.gz"
   sha256 "7c632bc4d50b9572b4f11f152deae5ad8c069a0f51fd1cb0648c1e72658b9c51"
   license "MIT"
 
