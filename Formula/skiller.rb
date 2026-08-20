0338f5863b6b4a209727e2c81c43dae6350c34c5
vlwkaos <mgparkprint@gmail.com>
2026-08-20 20:52:04 +0900

    skiller 0.2.0

--- Formula/skiller.rb
+++ Formula/skiller.rb
@@ -1,8 +1,8 @@
 class Skiller < Formula
   desc "Declarative project and global Agent Skills manager"
   homepage "https://github.com/vlwkaos/skiller"
-  url "https://github.com/vlwkaos/skiller/releases/download/v0.1.1/skiller-0.1.1-darwin-universal-b2d4dde.tar.gz"
-  sha256 "6d86e93e0f1c8c7dd8f9522a9045c50540fc805d55e6af81dbecce1c94f75497"
+  url "https://github.com/vlwkaos/skiller/releases/download/v0.2.0/skiller-0.2.0-darwin-universal.tar.gz"
+  sha256 "32410dca94a87bad13388e3a3515648a669b1eab6ef2b528dbf42d854f291e8e"
   license "MIT"
 
   def install
