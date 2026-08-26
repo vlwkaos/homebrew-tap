72f516b078971cbfe69e086fbf133441854d7668
vlwkaos <mgparkprint@gmail.com>
2026-08-26 16:36:48 +0900

    skiller 0.9.1

--- Formula/skiller.rb
+++ Formula/skiller.rb
@@ -1,14 +1,13 @@
 class Skiller < Formula
   desc "Declarative project and global skill management over the Vercel Skills CLI"
   homepage "https://github.com/vlwkaos/skiller"
-  url "https://github.com/vlwkaos/skiller/releases/download/v0.8.2/skiller-0.8.2-darwin-universal.tar.gz"
-  version "0.8.2"
-  sha256 "7a677f7cb805a52cc6960d1d01d757ef545d3350e655b903aba5a41b1210ad4d"
+  url "https://github.com/vlwkaos/skiller/releases/download/v0.9.1/skiller-0.9.1-darwin-universal.tar.gz"
+  sha256 "f7f06154fadb72a9a1af53e97233a7cdf87f8e4b31c8315b304a0e4e34e769b7"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/vlwkaos/skiller/releases/download/v0.8.2"
-    sha256 cellar: :any_skip_relocation, all: "31a59301e9d6790ee477e950160dd35f2840b6e8ebdf25cb8ae26d6ac6f16512"
+    root_url "https://github.com/vlwkaos/skiller/releases/download/v0.9.1"
+    sha256 cellar: :any_skip_relocation, all: "8d1c5c4b4c2eabdf4a93f34fb6f01b73cf6d8010487dcb462caa8731bc58a9bc"
   end
 
   def install
