d347d5034de99eaae809ec0b919fedaa76f282c7
vlwkaos <44766242+vlwkaos@users.noreply.github.com>
2026-09-11 17:54:00 +0900

    fix: use generated skiller bottle

--- Formula/skiller.rb
+++ Formula/skiller.rb
@@ -7,7 +7,8 @@ class Skiller < Formula
 
   bottle do
     root_url "https://github.com/vlwkaos/skiller/releases/download/v0.11.1"
-    sha256 cellar: :any_skip_relocation, all: "5bb5fabd3be6b8edeab8d7668b44c101bffbbac94db7aa2ac791fc1c1a5e076f"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3b06f293f74cb59c130105727d76a49d2075ac8b575e66ed9c1b2e108c30e185"
   end
 
   def install
